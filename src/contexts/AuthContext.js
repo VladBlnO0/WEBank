ad0bcb679222a88db821e1fad9803a7572e7219e
VlBl <vladpuku@gmail.com>
2025-04-12 22:46:46 +0300

    Squashed

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
             setIsAdmin(true);
             setUser("admin");
             return "admin";
-        }
+        }asd
         if (username === "user" && password === "user") {
             setIsAdmin(false);
             setUser("user");

d28588e631f161e821ae3c33142bd64029b2d0d7
VlBl <vladpuku@gmail.com>
2025-05-08 22:12:31 +0300

    Test

--- backend/api/stats/12.js
+++ backend/api/stats/12.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../../db');
-
-router.get('/', (req, res) => {
-    const sql = "SELECT COUNT(*) AS total_users FROM user.users WHERE role = 'user'";
-
-    db.query(sql, (err, results) => {
-        if (err) {
-            console.error('DB error:', err);
-            return res.status(500).json({ message: 'Server error' });
-        }
-
-        res.json({ totalUsers: results[0].total_users });
-    });
-});
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// const db = require('../../db');
+//
+// router.get('/', (req, res) => {
+//     const sql = "SELECT COUNT(*) AS total_users FROM user.users WHERE role = 'user'";
+//
+//     db.query(sql, (err, results) => {
+//         if (err) {
+//             console.error('DB error:', err);
+//             return res.status(500).json({ message: 'Server error' });
+//         }
+//
+//         res.json({ totalUsers: results[0].total_users });
+//     });
+// });
+//
+// module.exports = router;

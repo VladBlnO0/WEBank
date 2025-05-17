277f4f125a0e66d8a698ca4c03b166daa3358c02
VlBl <vladpuku@gmail.com>
2025-05-17 14:33:30 +0300

    test master 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,6 @@ import User from './pages/user/User'
 import UserTransfer from './pages/user/UserTransfer'
 import UserServices from './pages/user/UserServices'
 
-import Admin from './pages/admin/Admin'
-import AdminUsers from './pages/admin/AdminUsers'
-import AdminMainpage from './pages/admin/AdminMainpage'
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -20,8 +16,8 @@ function App() {
                     <Route path="/404" element={<NoPage />} />
 
                     <Route path="/" element={<User />} />
-                    <Route path="/user-transfer" element={<UserTransfer />} />
-                    <Route path="/user-services" element={<UserServices />} />
+                    <Route path="/transfer" element={<UserTransfer />} />
+                    <Route path="/services" element={<UserServices />} />
                 </Routes>
             </Router>
         </div>
